7d9af7c7301183de088260cc58be38041db6c057
AntonKochubeev <160320922+AntonKochubeev@users.noreply.github.com>
2024-03-10 21:22:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -35,7 +35,7 @@ double sinn(double x, uint16_t count) {
         if ((i - 1) % 2 != 0) {
             a = -1;
         }
-        sum += a * calcItem(x, 2 * i - 1);   
+  sum += a * calcItem(x, 2 * i - 1);   
     }
     return sum;
 }
@@ -47,7 +47,7 @@ double cosn(double x, uint16_t count) {
         if ((i - 1) % 2 != 0) {
              a = -1;
         }
-        sum += a * calcItem(x, 2 * i - 2);   
+  sum += a * calcItem(x, 2 * i - 2);   
     }
     return sum;
 }
