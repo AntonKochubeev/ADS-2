b2105c7cd564f5cfb5e3f0a892bf153ded70fa35
AntonKochubeev <160320922+AntonKochubeev@users.noreply.github.com>
2024-03-10 21:26:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -29,25 +29,25 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-   double sum = 0;
+  double sum = 0;
     for (uint32_t i = 0; i <= count; i++) {
          double a = 1;
         if ((i - 1) % 2 != 0) {
              a = -1;
         }
-      sum += a * calcItem(x, 2 * i - 1);   
+    sum += a * calcItem(x, 2 * i - 1);   
     }
     return sum;
 }
 
 double cosn(double x, uint16_t count) {
-    double sum = 0;
+  double sum = 0;
     for (uint32_t i = 0; i <= count; i++) {
          double a = 1;
         if ((i - 1) % 2 != 0) {
              a = -1;
         }
-      sum += a * calcItem(x, 2 * i - 2);   
+    sum += a * calcItem(x, 2 * i - 2);   
     }
     return sum;
 }
